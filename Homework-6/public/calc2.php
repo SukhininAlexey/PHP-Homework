744c1f4f0f4ff6b557ca83eb3b578d36c4430bd1
SukhininAlexey <sukhininalexey@gmail.com>
2018-04-05 20:59:17 +0300

    Калькуляторы.

---
+++ Homework-6/public/calc2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Calc</title>
+</head>
+<body>
+    <form action="" enctype="multipart/form-data" method="post">
+    <p>Операнд 1: <input type="number" name="oper1"></p>
+    <p>Операнд 2: <input type="number" name="oper2"></p>
+    <input type="submit" name="operation" value="+">
+    <input type="submit" name="operation" value="-">
+    <input type="submit" name="operation" value="*">
+    <input type="submit" name="operation" value="/">
+</form>
+</body>
+</html>
+
+<?php        
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $oper1 = (int)$_POST["oper1"];
+    $oper2 = (int)$_POST["oper2"];
+    $operation = $_POST["operation"];
+    $result;
+    switch ($operation){
+        case "+":
+            $result = $oper1 + $oper2;
+            break;
+        case "-":
+            $result = $oper1 - $oper2;
+            break;
+        case "*":
+            $result = $oper1 * $oper2;
+            break;
+        case "/":
+            if($oper2){
+                $result = $oper1 / $oper2;
+            }else{
+                $result = "Divided by zero!";
+            }
+            break;
+    }
+    echo "<p>Результат: " . $oper1 . " " . $operation . " " . $oper2 . " = " . $result . "</p>";    
+}
+?>
